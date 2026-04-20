12f652a84177a0c37295f12aba050a98c171aa52
dev1-kakaobank <137971421+dev1-kakaobank@users.noreply.github.com>
2026-04-20 16:19:46 +0900

    Add 'id' field to SmsSendRequest record

--- src/main/java/com/codingtest/nss/sender/dto/SmsSendRequest.java
+++ src/main/java/com/codingtest/nss/sender/dto/SmsSendRequest.java
@@ -1,4 +1,4 @@
 package com.codingtest.nss.sender.dto;
 
-public record SmsSendRequest(String phoneNumber, String title, String contents) {
+public record SmsSendRequest(String id, String phoneNumber, String title, String contents) {
 }
